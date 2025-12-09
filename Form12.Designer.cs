dcd168af9b11c0bf63e4db6448441a68db21b6cb
Molodoy377 <dyzeve@gmail.com>
2025-12-10 00:36:42 +0300

    Добавление обработчика событий кнопки в 12 форму

--- Form12.Designer.cs
+++ Form12.Designer.cs
@@ -67,6 +67,8 @@
             this.button1.TabIndex = 12;
             this.button1.Text = "Вернуться на главную";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
             // 
             // Form12
             // 
