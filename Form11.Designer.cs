71b102e722659c981d8552aacd943697d5d9df91
Molodoy377 <dyzeve@gmail.com>
2025-12-09 23:54:25 +0300

    Добавление обработчика событий в 11 форме

--- Form11.Designer.cs
+++ Form11.Designer.cs
@@ -91,6 +91,8 @@
             this.button1.TabIndex = 11;
             this.button1.Text = "Вернуться на главную";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
             // 
             // Form11
             // 
