866d6f387c5adb1a85c45b58a4d4d3cde1d3e7e5
Molodoy377 <dyzeve@gmail.com>
2025-12-10 15:09:02 +0300

    Добавление функционала для первой задачи

--- Form14.Designer.cs
+++ Form14.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form14));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.MintCream;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(82, 9);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(38, 16, 38, 16);
+            this.label1.Size = new System.Drawing.Size(1250, 78);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Разработать класс «Х». Привести примеры использования класса в\r\nпостроении иер" +
+    "архии наследования и композиции.";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.MintCream;
+            this.textBox1.Location = new System.Drawing.Point(82, 90);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(620, 358);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.MintCream;
+            this.textBox2.Location = new System.Drawing.Point(712, 90);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(620, 358);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Stencil", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(474, 454);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(439, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Показать правильный ответ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form14
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form14";
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1444, 861);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form14";
+            this.Text = "Практические задачи";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Form14.cs
+++ Form14.cs
@@ -12,9 +12,21 @@ namespace MultyMediaJAVA
 {
     public partial class Form14 : Form
     {
+        private const string CorrectAnswer = "using System;\r\nusing System.Collections.Generic;\r\n\r\npublic class X\r\n{\r\n    private List<int> values = new List<int>();\r\n    \r\n    public void Add(int value)\r\n    {\r\n        values.Add(value);\r\n    }\r\n    \r\n    public bool Remove(int value)\r\n    {\r\n        return values.Remove(value);\r\n    }\r\n    \r\n    public int Count => values.Count;\r\n    \r\n    public int[] GetAll()\r\n    {\r\n        return values.ToArray();\r\n    }\r\n}\r\n";
         public Form14()
         {
             InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // При старте очищаем TextBox
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = CorrectAnswer;
         }
     }
 }
